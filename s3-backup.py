2a01eb545bec344a7e97a4f951e6c2240cc5ae89
Maxim Rychkov <rychkov03@yandex.ru>
2025-04-04 04:03:24 -0700

    feat: restore from archive

--- s3-backup.py
+++ s3-backup.py
@@ -1,4 +1,4 @@
-import rclone
+from rclone_python import rclone
 import os
 import shutil
 from datetime import datetime
@@ -6,20 +6,12 @@ from datetime import datetime
 
 def main():
 
-    cfg_path = r'.rclone.conf'
+    
 
-    with open(cfg_path) as f:
-        cfg = f.read()
+    shutil.unpack_archive("test.zip", "/tmp/backup")
 
-    archive_name = "s3-backup" + '_' + datetime.strftime(datetime.utcnow(), "%Y.%m.%d.%H-%M-%S") + 'UTC' + '.backup'
+    rclone.copy("/tmp/backup", "vk:pelican-media-assets")
 
-    # s3-1:pelican-local-env is source
-    rclone.with_config(cfg).copy("s3-1:pelican-local-env", "/tmp/s3-backup", flags=["--transfers=256"])
-
-    shutil.make_archive(archive_name, 'zip', "/tmp/s3-backup")
-
-    # s3-2:backup is destination
-    rclone.with_config(cfg).copy(archive_name + ".zip", "s3-2:backup", flags=["--transfers=256"])
 
 
 if __name__ == '__main__':
