f4272759f1b92c021ddbb35f27d76172a9729856
Yam1x <rychkov03@yandex.ru>
2025-04-07 09:31:08 +0000

    feat: add custom helm chart

--- s3-backup.py
+++ s3-backup.py
@@ -5,12 +5,9 @@ from datetime import datetime
 
 
 def main():
-
     directory_path = "/tmp/backup"
 
-   # os.mkdir(directory_path)
-
-    archive_name = "s3-backup" + '_' + datetime.strftime(datetime.utcnow(), "%Y.%m.%d.%H-%M-%S") + 'UTC' + '.backup'
+    archive_name = "s3-backup" + '_' + datetime.strftime(datetime.utcnow(), "%Y.%m.%d.%H-%M-%S") + 'UTC'
     
     s3_download = boto3.client(
         's3',
@@ -30,16 +27,20 @@ def main():
     
     bucket_name_upload = os.getenv('AWS_BUCKET_NAME_UPLOAD')
     
-    download_dir("", directory_path, bucket_name_download, s3_download)
+    download_dir(directory_path, bucket_name_download, s3_download)
 
-    shutil.make_archive(archive_name, 'zip', directory_path)
+    if os.path.exists(directory_path):
 
-    upload_to_s3(archive_name + ".zip", archive_name + ".zip", s3_upload, bucket_name_upload)
+        shutil.make_archive(archive_name, 'zip', directory_path)
 
-def download_dir(prefix, local, bucket, client):
+        upload_to_s3(archive_name + ".zip", s3_upload, bucket_name_upload)
+
+        shutil.rmtree(directory_path)
+        os.remove(archive_name + ".zip")
+
+def download_dir(local, bucket, client):
     """
     params:
-    - prefix: pattern to match in s3
     - local: local path to folder in which to place files
     - bucket: s3 bucket with target contents
     - client: initialized s3 client object
@@ -49,38 +50,47 @@ def download_dir(prefix, local, bucket, client):
     next_token = ''
     base_kwargs = {
         'Bucket':bucket,
-        'Prefix':prefix,
     }
+
     while next_token is not None:
         kwargs = base_kwargs.copy()
+
         if next_token != '':
             kwargs.update({'ContinuationToken': next_token})
+        
         results = client.list_objects_v2(**kwargs)
+
         contents = results.get('Contents')
-        for i in contents:
-            k = i.get('Key')
-            if k[-1] != '/':
-                keys.append(k)
-            else:
-                dirs.append(k)
+
+        if contents is not None:
+            for i in contents:
+                k = i.get('Key')
+                if k[-1] != '/':
+                    keys.append(k)
+                else:
+                    dirs.append(k)
+
         next_token = results.get('NextContinuationToken')
+
     for d in dirs:
         dest_pathname = os.path.join(local, d)
+
         if not os.path.exists(os.path.dirname(dest_pathname)):
             os.makedirs(os.path.dirname(dest_pathname))
+
     for k in keys:
         dest_pathname = os.path.join(local, k)
+
         if not os.path.exists(os.path.dirname(dest_pathname)):
             os.makedirs(os.path.dirname(dest_pathname))
+        
         client.download_file(bucket, k, dest_pathname)
 
      
-def upload_to_s3(source_path, destination_filename, s3, bucket):
-    with open(source_path, "rb") as data:
-        s3.upload_fileobj(data, bucket, destination_filename)
+def upload_to_s3(path, s3, bucket):
+    with open(path, "rb") as data:
+        s3.upload_fileobj(data, bucket, path)
     
-
-
     
 if __name__ == '__main__':
 
