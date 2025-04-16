442d5ad4ceea2b82b9d44b2dfa306c19ec1b77a8
Yam1x <rychkov03@yandex.ru>
2025-04-16 21:12:34 +0500

    chore: add variable for backup filename

--- s3-backup.py
+++ s3-backup.py
@@ -10,17 +10,19 @@ def main():
 
     bucket="s3:pelican-local-env"
 
+    backup_filename = "test.zip"
+
     if(os.path.exists(path)):
         if(not os.listdir(path)):
             print("нет файлов")
-            shutil.unpack_archive("test.zip", "/tmp/backup")
+            shutil.unpack_archive(backup_filename, "/tmp/backup")
             print(os.listdir(path))
             rclone.copy("/tmp/backup", bucket)
             shutil.rmtree(path)
     else:
         print("нет файлов2")
         os.mkdir(path)
-        shutil.unpack_archive("test.zip", "/tmp/backup")
+        shutil.unpack_archive(backup_filename, "/tmp/backup")
         print(os.listdir(path))
         rclone.copy("/tmp/backup", bucket)
         shutil.rmtree(path)
