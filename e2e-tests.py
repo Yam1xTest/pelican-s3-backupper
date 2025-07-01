331d28441d4ac3536785ad02a8096c24e8299fbb
Maxim Rychkov <81160491+Yam1x@users.noreply.github.com>
2025-07-01 13:07:08 +0500

    Update e2e-tests.py

--- e2e-tests.py
+++ e2e-tests.py
@@ -4,9 +4,9 @@ import os
 def main():
     s3 = boto3.client(
         's3',
-        aws_access_key_id=os.getenv('DESTINATION_DB_AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('DESTINATION_DB_AWS_SECRET_ACCESS_KEY'),
-        endpoint_url=os.getenv('DESTINATION_DB_AWS_ENDPOINT')
+        aws_access_key_id=os.getenv('DESTINATION_S3_AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('DESTINATION_S3_AWS_SECRET_ACCESS_KEY'),
+        endpoint_url=os.getenv('DESTINATION_S3_AWS_ENDPOINT')
     )
 
     objects_list = s3.list_objects_v2(Bucket="pelican-backups")
