741abbd8cc404772f7099c4c442f031f3b45200f
Maxim Rychkov <81160491+Yam1x@users.noreply.github.com>
2025-07-01 11:53:46 +0500

    Create e2e-tests.py

---
+++ e2e-tests.py
@@ -0,0 +1,24 @@
+import boto3
+import os
+
+def main():
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.getenv('DESTINATION_DB_AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('DESTINATION_DB_AWS_SECRET_ACCESS_KEY'),
+        endpoint_url=os.getenv('DESTINATION_DB_AWS_ENDPOINT')
+    )
+
+    objects_list = s3.list_objects_v2(Bucket="pelican-backups")
+    try:
+        contents = objects_list["Contents"]
+        last_backup_size = contents[-1]['Size']
+
+    except:
+        raise Exception("Bucket is empty")
+
+    if(last_backup_size == 0):
+        raise Exception("Backup size is 0")
+
+if __name__ == '__main__':
+    main()
